461fc7f52f2c47f35fec90d3d372bc2a30b062a3
Goran Mekić <meka@tilda.center>
2021-05-06 20:22:50 +0200

    Use indent to format code

--- share/examples/sound/basic.c
+++ share/examples/sound/basic.c
@@ -3,46 +3,49 @@
 int
 main()
 {
-  config_t config = {
-    .device = "/dev/dsp",
-    .channels = -1,
-    .format = format,
-    .frag = -1,
-    .sample_rate = 48000,
-    .sample_size = sizeof(sample_t),
-    .buffer_info.fragments = -1,
-    .mmap = 0,
-  };
+	config_t config = {
+		.device = "/dev/dsp",
+		.channels = -1,
+		.format = format,
+		.frag = -1,
+		.sample_rate = 48000,
+		.sample_size = sizeof(sample_t),
+		.buffer_info.fragments = -1,
+		.mmap = 0,
+	};
 
-  /* Initialize device */
-  oss_init(&config);
+	/* Initialize device */
+	oss_init(&config);
 
-  /* Allocate input and output buffers so that their size match frag_size */
-  int bytes = config.buffer_info.bytes;
-  int8_t ibuf[bytes];
-  int8_t obuf[bytes];
-  sample_t *channels = (sample_t *)malloc(bytes);
-  printf(
-    "bytes: %d, fragments: %d, fragsize: %d, fragstotal: %d, samples: %d\n",
-    bytes,
-    config.buffer_info.fragments,
-    config.buffer_info.fragsize,
-    config.buffer_info.fragstotal,
-    config.sample_count
-  );
+	/*
+	 * Allocate input and output buffers so that their size match
+	 * frag_size
+	 */
+	int bytes = config.buffer_info.bytes;
+	int8_t ibuf[bytes];
+	int8_t obuf[bytes];
+	sample_t *channels = (sample_t *)malloc(bytes);
 
-  /* Minimal engine: read input and copy it to the output */
-  while(1)
-  {
-    read(config.fd, ibuf, bytes);
-    oss_split(&config, (sample_t *)ibuf, channels);
-    /* All processing will happen here */
-    oss_merge(&config, channels, (sample_t *)obuf);
-    write(config.fd, obuf, bytes);
-  }
+	printf(
+	    "bytes: %d, fragments: %d, fragsize: %d, fragstotal: %d, samples: %d\n",
+	    bytes,
+	    config.buffer_info.fragments,
+	    config.buffer_info.fragsize,
+	    config.buffer_info.fragstotal,
+	    config.sample_count
+	    );
 
-  /* Cleanup */
-  free(channels);
-  close(config.fd);
-  return 0;
+	/* Minimal engine: read input and copy it to the output */
+	while (1) {
+		read(config.fd, ibuf, bytes);
+		oss_split(&config, (sample_t *)ibuf, channels);
+		/* All processing will happen here */
+		oss_merge(&config, channels, (sample_t *)obuf);
+		write(config.fd, obuf, bytes);
+	}
+
+	/* Cleanup */
+	free(channels);
+	close(config.fd);
+	return 0;
 }
--- share/examples/sound/ossinit.h
+++ share/examples/sound/ossinit.h
@@ -14,17 +14,18 @@
 /* Format can be unsigned, in which case replace S with U */
 #if SAMPLE_SIZE == 32
 typedef int32_t sample_t;
-int format = AFMT_S32_NE; /* Signed 32bit native endian format */
+int	format = AFMT_S32_NE;		/* Signed 32bit native endian format */
 #elif SAMPLE_SIZE == 16
 typedef int16_t sample_t;
-int format = AFMT_S16_NE; /* Signed 16bit native endian format */
+int	format = AFMT_S16_NE;		/* Signed 16bit native endian format */
 #elif SAMPLE_SIZE == 8
 typedef int8_t sample_t;
-int format = AFMT_S8_NE; /* Signed 8bit native endian format */
+int	format = AFMT_S8_NE;		/* Signed 8bit native endian format */
 #else
 #error Unsupported sample format!
 typedef int32_t sample_t;
-int format = AFMT_S32_NE; /* Not a real value, just silencing compiler errors */
+int	format = AFMT_S32_NE;		/* Not a real value, just silencing
+					 * compiler errors */
 #endif
 
 
@@ -34,20 +35,19 @@ int format = AFMT_S32_NE; /* Not a real value, just silencing compiler errors */
  * For real world applications, this structure will probably contain many
  * more fields
  */
-typedef struct config
-{
-  char *device;
-  int channels;
-  int fd;
-  int format;
-  int frag;
-  int sample_count;
-  int sample_rate;
-  int sample_size;
-  int chsamples;
-  int mmap;
-  oss_audioinfo audio_info;
-  audio_buf_info buffer_info;
+typedef struct config {
+	char   *device;
+	int	channels;
+	int	fd;
+	int	format;
+	int	frag;
+	int	sample_count;
+	int	sample_rate;
+	int	sample_size;
+	int	chsamples;
+	int	mmap;
+	oss_audioinfo audio_info;
+	audio_buf_info buffer_info;
 } config_t;
 
 
@@ -58,11 +58,10 @@ typedef struct config
 void
 check_error(const int value, const char *message)
 {
-  if (value == -1)
-  {
-    fprintf(stderr, "OSS error: %s %s\n", message, strerror(errno));
-    exit(1);
-  }
+	if (value == -1) {
+		fprintf(stderr, "OSS error: %s %s\n", message, strerror(errno));
+		exit(1);
+	}
 }
 
 
@@ -71,9 +70,12 @@ check_error(const int value, const char *message)
 int
 size2frag(int x)
 {
-  int frag = 0;
-  while ((1 << frag) < x) { ++frag; }
-  return frag;
+	int frag = 0;
+
+	while ((1 << frag) < x) {
+		++frag;
+	}
+	return frag;
 }
 
 
@@ -86,14 +88,14 @@ size2frag(int x)
 void
 oss_split(config_t *config, sample_t *input, sample_t *output)
 {
-  int channel;
-  int index;
-  for (int i = 0; i < config->sample_count; ++i)
-  {
-    channel = i % config->channels;
-    index = i / config->channels;
-    output[channel * index] = input[i];
-  }
+	int channel;
+	int index;
+
+	for (int i = 0; i < config->sample_count; ++i) {
+		channel = i % config->channels;
+		index = i / config->channels;
+		output[channel * index] = input[i];
+	}
 }
 
 
@@ -104,127 +106,122 @@ oss_split(config_t *config, sample_t *input, sample_t *output)
 void
 oss_merge(config_t *config, sample_t *input, sample_t *output)
 {
-  for (int channel = 0; channel < config->channels; ++channel)
-  {
-    for (int index = 0; index < config->chsamples ; ++index)
-    {
-      output[index * config->channels + channel] = input[channel * index];
-    }
-  }
+	for (int channel = 0; channel < config->channels; ++channel) {
+		for (int index = 0; index < config->chsamples; ++index) {
+			output[index * config->channels + channel] = input[channel * index];
+		}
+	}
 }
 
 void
 oss_init(config_t *config)
 {
-  int error;
-  int tmp;
-  /* Open the device for read and write */
-  config->fd = open(config->device, O_RDWR);
-  check_error(config->fd, "open");
+	int error;
+	int tmp;
 
-  /* Get device information */
-  config->audio_info.dev = -1;
-  error = ioctl(config->fd, SNDCTL_ENGINEINFO, &(config->audio_info));
-  check_error(error, "SNDCTL_ENGINEINFO");
-  printf("min_channels: %d\n", config->audio_info.min_channels);
-  printf("max_channels: %d\n", config->audio_info.max_channels);
-  printf("latency: %d\n", config->audio_info.latency);
-  printf("handle: %s\n", config->audio_info.handle);
-  if (config->audio_info.min_rate > config->sample_rate || config->sample_rate > config->audio_info.max_rate)
-  {
-    fprintf(stderr, "%s doesn't support chosen ", config->device);
-    fprintf(stderr, "samplerate of %dHz!\n", config->sample_rate);
-    exit(1);
-  }
-  if (config->channels < 1)
-  {
-    config->channels = config->audio_info.max_channels;
-  }
+	/* Open the device for read and write */
+	config->fd = open(config->device, O_RDWR);
+	check_error(config->fd, "open");
 
-  /*
-   * If device is going to be used in mmap mode, disable all format
-   * conversions. Official OSS documentation states error code should not be
-   * checked. http://manuals.opensound.com/developer/mmap_test.c.html#LOC10
-   */
-  if (config->mmap)
-  {
-    tmp = 0;
-    ioctl (config->fd, SNDCTL_DSP_COOKEDMODE, &tmp);
-  }
+	/* Get device information */
+	config->audio_info.dev = -1;
+	error = ioctl(config->fd, SNDCTL_ENGINEINFO, &(config->audio_info));
+	check_error(error, "SNDCTL_ENGINEINFO");
+	printf("min_channels: %d\n", config->audio_info.min_channels);
+	printf("max_channels: %d\n", config->audio_info.max_channels);
+	printf("latency: %d\n", config->audio_info.latency);
+	printf("handle: %s\n", config->audio_info.handle);
+	if (config->audio_info.min_rate > config->sample_rate || config->sample_rate > config->audio_info.max_rate) {
+		fprintf(stderr, "%s doesn't support chosen ", config->device);
+		fprintf(stderr, "samplerate of %dHz!\n", config->sample_rate);
+		exit(1);
+	}
+	if (config->channels < 1) {
+		config->channels = config->audio_info.max_channels;
+	}
 
-  /*
-   * Set number of channels. If number of channels is chosen to the value
-   * near the one wanted, save it in config
-   */
-  tmp = config->channels;
-  error = ioctl(config->fd, SNDCTL_DSP_CHANNELS, &tmp);
-  check_error(error, "SNDCTL_DSP_CHANNELS");
-  if (tmp != config->channels) /* or check if tmp is close enough? */
-  {
-    fprintf(stderr, "%s doesn't support chosen ", config->device);
-    fprintf(stderr, "channel count of %d", config->channels);
-    fprintf(stderr, ", set to %d!\n", tmp);
-  }
-  config->channels = tmp;
+	/*
+         * If device is going to be used in mmap mode, disable all format
+         * conversions. Official OSS documentation states error code should not be
+         * checked. http://manuals.opensound.com/developer/mmap_test.c.html#LOC10
+         */
+	if (config->mmap) {
+		tmp = 0;
+		ioctl(config->fd, SNDCTL_DSP_COOKEDMODE, &tmp);
+	}
 
-  /* Set format, or bit size: 8, 16, 24 or 32 bit sample */
-  tmp = config->format;
-  error = ioctl(config->fd, SNDCTL_DSP_SETFMT, &tmp);
-  check_error(error, "SNDCTL_DSP_SETFMT");
-  if (tmp != config->format)
-  {
-    fprintf(stderr, "%s doesn't support chosen sample format!\n", config->device);
-    exit(1);
-  }
+	/*
+         * Set number of channels. If number of channels is chosen to the value
+         * near the one wanted, save it in config
+         */
+	tmp = config->channels;
+	error = ioctl(config->fd, SNDCTL_DSP_CHANNELS, &tmp);
+	check_error(error, "SNDCTL_DSP_CHANNELS");
+	if (tmp != config->channels) {	/* or check if tmp is close enough? */
+		fprintf(stderr, "%s doesn't support chosen ", config->device);
+		fprintf(stderr, "channel count of %d", config->channels);
+		fprintf(stderr, ", set to %d!\n", tmp);
+	}
+	config->channels = tmp;
 
-  /* Most common values for samplerate (in kHz): 44.1, 48, 88.2, 96 */
-  tmp = config->sample_rate;
-  error = ioctl(config->fd, SNDCTL_DSP_SPEED, &tmp);
-  check_error(error, "SNDCTL_DSP_SPEED");
+	/* Set format, or bit size: 8, 16, 24 or 32 bit sample */
+	tmp = config->format;
+	error = ioctl(config->fd, SNDCTL_DSP_SETFMT, &tmp);
+	check_error(error, "SNDCTL_DSP_SETFMT");
+	if (tmp != config->format) {
+		fprintf(stderr, "%s doesn't support chosen sample format!\n", config->device);
+		exit(1);
+	}
 
-  /* Get and check device capabilities */
-  error = ioctl(config->fd, SNDCTL_DSP_GETCAPS, &(config->audio_info.caps));
-  check_error(error, "SNDCTL_DSP_GETCAPS");
-  if (!(config->audio_info.caps & PCM_CAP_DUPLEX))
-  {
-    fprintf(stderr, "Device doesn't support full duplex!\n");
-    exit(1);
-  }
-  if (config->mmap)
-  {
-    if (!(config->audio_info.caps & PCM_CAP_TRIGGER))
-    {
-      fprintf(stderr, "Device doesn't support triggering!\n");
-      exit(1);
-    }
-    if (!(config->audio_info.caps & PCM_CAP_MMAP))
-    {
-      fprintf(stderr, "Device doesn't support mmap mode!\n");
-      exit(1);
-    }
-  }
+	/* Most common values for samplerate (in kHz): 44.1, 48, 88.2, 96 */
+	tmp = config->sample_rate;
+	error = ioctl(config->fd, SNDCTL_DSP_SPEED, &tmp);
+	check_error(error, "SNDCTL_DSP_SPEED");
 
-  /*
-   * If desired frag is smaller than minimum, based on number of channels
-   * and format (size in bits: 8, 16, 24, 32), set that as frag. Buffer size
-   * is 2^frag, but the real size of the buffer will be read when the
-   * configuration of the device is successfull
-   */
-  int min_frag = size2frag(config->sample_size * config->channels);
-  if (config->frag < min_frag) { config->frag = min_frag; }
+	/* Get and check device capabilities */
+	error = ioctl(config->fd, SNDCTL_DSP_GETCAPS, &(config->audio_info.caps));
+	check_error(error, "SNDCTL_DSP_GETCAPS");
+	if (!(config->audio_info.caps & PCM_CAP_DUPLEX)) {
+		fprintf(stderr, "Device doesn't support full duplex!\n");
+		exit(1);
+	}
+	if (config->mmap) {
+		if (!(config->audio_info.caps & PCM_CAP_TRIGGER)) {
+			fprintf(stderr, "Device doesn't support triggering!\n");
+			exit(1);
+		}
+		if (!(config->audio_info.caps & PCM_CAP_MMAP)) {
+			fprintf(stderr, "Device doesn't support mmap mode!\n");
+			exit(1);
+		}
+	}
 
-  /*
-   * Allocate buffer in fragments. Total buffer will be split in number
-   * of fragments (2 by default)
-   */
-  if (config->buffer_info.fragments < 0) { config->buffer_info.fragments = 2; }
-  tmp = ((config->buffer_info.fragments) << 16) | config->frag;
-  error = ioctl(config->fd, SNDCTL_DSP_SETFRAGMENT, &tmp);
-  check_error(error, "SNDCTL_DSP_SETFRAGMENT");
+	/*
+         * If desired frag is smaller than minimum, based on number of channels
+         * and format (size in bits: 8, 16, 24, 32), set that as frag. Buffer size
+         * is 2^frag, but the real size of the buffer will be read when the
+         * configuration of the device is successfull
+         */
+	int min_frag = size2frag(config->sample_size * config->channels);
 
-  /* When all is set and ready to go, get the size of buffer */
-  error = ioctl(config->fd, SNDCTL_DSP_GETOSPACE, &(config->buffer_info));
-  check_error(error, "SNDCTL_DSP_GETOSPACE");
-  config->sample_count = config->buffer_info.bytes / config->sample_size;
-  config->chsamples =  config->sample_count / config->channels;
+	if (config->frag < min_frag) {
+		config->frag = min_frag;
+	}
+
+	/*
+         * Allocate buffer in fragments. Total buffer will be split in number
+         * of fragments (2 by default)
+         */
+	if (config->buffer_info.fragments < 0) {
+		config->buffer_info.fragments = 2;
+	}
+	tmp = ((config->buffer_info.fragments) << 16) | config->frag;
+	error = ioctl(config->fd, SNDCTL_DSP_SETFRAGMENT, &tmp);
+	check_error(error, "SNDCTL_DSP_SETFRAGMENT");
+
+	/* When all is set and ready to go, get the size of buffer */
+	error = ioctl(config->fd, SNDCTL_DSP_GETOSPACE, &(config->buffer_info));
+	check_error(error, "SNDCTL_DSP_GETOSPACE");
+	config->sample_count = config->buffer_info.bytes / config->sample_size;
+	config->chsamples = config->sample_count / config->channels;
 }
